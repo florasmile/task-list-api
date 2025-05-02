92c2bcc2fc851c49b02981ba7d1fec885f2a16db
dehuihu <dehuihu@gmail.com>
2025-05-01 22:54:02 -0700

    Adds get_all, get_one, update, delete endpoints and passed all tests in wave_01

--- app/models/task.py
+++ app/models/task.py
@@ -12,20 +12,40 @@ class Task(db.Model):
  #create a task instance from a dictionary
     @classmethod
     def from_dict(cls, task_data):
-        new_task = cls(
-            title=task_data["title"],
-            description=task_data["description"],
-            completed_at=task_data["completed_at"] 
-        )
+        if "completed_at" in task_data:
+            new_task = cls(
+                title=task_data["title"],
+                description=task_data["description"],
+                completed_at=task_data["completed_at"] 
+            )
+        else:
+            new_task = cls(
+                title=task_data["title"],
+                description=task_data["description"]
+            )
 
         return new_task
     
     # Return a Python dictionary from a model instance
     def to_dict(self):
+        if not self.completed_at:
+            return {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": False
+            }
+        else:
+            return {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "completed_at": self.completed_at
+            }
+    
+    def to_nested_dict(self):
         return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "completed_at": self.completed_at
+            "task": self.to_dict()
         }
+
 
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+from wsgiref import validate
+from flask import Blueprint, request, Response
 from app.models.task import Task
 from ..db import db
 from .route_utilities import validate_model, create_model
@@ -14,5 +15,40 @@ def create_task():
     db.session.add(new_task)
     db.session.commit()
     
-    return new_task.to_dict(), 201
+    return new_task.to_nested_dict(), 201
 
+@bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+
+    tasks = db.session.scalars(query)
+
+    return [task.to_dict() for task in tasks]
+
+@bp.put("/<id>")
+def update_task(id):
+    task = validate_model(Task, id)
+
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
+
+@bp.get("/<id>")
+def get_task(id):
+    task = validate_model(Task, id)
+
+    return task.to_nested_dict()
+
+@bp.delete("<id>")
+def delete_task(id):
+    task = validate_model(Task, id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
