52ce3f25cd77879b1a6c04f2a3a01f082fa12b18
dehuihu <dehuihu@gmail.com>
2025-05-03 15:04:35 -0700

    Refactors to_dict and from_dict for task model in wave_01

--- app/models/task.py
+++ app/models/task.py
@@ -12,36 +12,29 @@ class Task(db.Model):
  #create a task instance from a dictionary
     @classmethod
     def from_dict(cls, task_data):
-        if "completed_at" in task_data:
-            new_task = cls(
-                title=task_data["title"],
-                description=task_data["description"],
-                completed_at=task_data["completed_at"] 
-            )
-        else:
-            new_task = cls(
+        new_task = cls(
                 title=task_data["title"],
                 description=task_data["description"]
-            )
+        )
+        if "completed_at" in task_data:
+            new_task.completed_at = task_data["completed_at"] 
 
         return new_task
     
     # Return a Python dictionary from a model instance
     def to_dict(self):
+        response_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
+
         if not self.completed_at:
-            return {
-                "id": self.id,
-                "title": self.title,
-                "description": self.description,
-                "is_complete": False
-            }
+            response_dict["is_complete"] = False
         else:
-            return {
-                "id": self.id,
-                "title": self.title,
-                "description": self.description,
-                "completed_at": self.completed_at
-            }
+            response_dict["completed_at"] = self.completed_at
+
+        return response_dict
     
     def to_nested_dict(self):
         return {
