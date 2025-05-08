452848dfc943749e64d529cae3c3a03aea187c55
dehuihu <dehuihu@gmail.com>
2025-05-08 12:02:38 -0700

    Adds goal model in wave_05

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,18 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+
+     #create an instance from a dictionary
+    @classmethod
+    def from_dict(cls, goal_data):
+        return cls(
+                title=goal_data["title"]
+        )
+    
+    # Return a Python dictionary from a model instance
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title
+        }
--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,1 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request, Response
+from app.models.goal import Goal
+from ..db import db
+from .route_utilities import validate_model, create_model
+
+#create bp
+bp = Blueprint("bp", __name__, url_prefix="/goals")
+
+@bp.post("")
+def create_goal():
+    request_body = request.get_json()
+    new_goal = create_model(Goal, request_body)
+
+    db.session.add(new_goal)
+    db.session.commit()
+    
+    response = {
+        "goal": new_goal.to_dict()
+    }
+    return response, 201
